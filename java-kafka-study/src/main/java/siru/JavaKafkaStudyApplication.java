16149a140825dbe504a3e0d5496765e874c5df50
shinwoo <sinwoo1225@gmail.com>
2021-12-13 23:09:58 +0900

    카프카 자바 예제 프로젝트 생성

---
+++ java-kafka-study/src/main/java/siru/JavaKafkaStudyApplication.java
@@ -0,0 +1,13 @@
+package siru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKafkaStudyApplication.class, args);
+    }
+
+}
---
+++ java-kafka-study/src/test/java/siru/JavaKafkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package siru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
